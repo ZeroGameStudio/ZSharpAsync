77421d03212fff8c72b0f97fb12a50d7cbefe832
Lin7Han <941532182@qq.com>
2024-09-16 09:48:20 +0800

    Update AccumulatedSeconds.cs

--- Source/Managed/ZeroGames.ZSharp.Async/Source/AccumulatedSeconds.cs
+++ Source/Managed/ZeroGames.ZSharp.Async/Source/AccumulatedSeconds.cs
@@ -9,21 +9,21 @@ public readonly struct AccumulatedSeconds
 	public static AccumulatedSeconds operator +(AccumulatedSeconds lhs, double deltaSeconds) => new(lhs._rounds, lhs._seconds + deltaSeconds);
 
 	public TimeSpan TimeSpan => TimeSpan.FromSeconds(Seconds);
-	public double Seconds => _secondsPerRound * _rounds + _seconds;
+	public double Seconds => SECONDS_PER_ROUND * _rounds + _seconds;
 
 	private AccumulatedSeconds(uint64 rounds, double seconds)
 	{
 		ArgumentOutOfRangeException.ThrowIfNegative(seconds);
 		
-		uint64 extraRounds = (uint64)(seconds / _secondsPerRound);
+		uint64 extraRounds = (uint64)(seconds / SECONDS_PER_ROUND);
 		rounds += extraRounds;
-		seconds -= _secondsPerRound * extraRounds;
+		seconds -= SECONDS_PER_ROUND * extraRounds;
 		
 		_rounds = rounds;
 		_seconds = seconds;
 	}
 
-	private const double _secondsPerRound = 1000.0;
+	private const double SECONDS_PER_ROUND = 1000.0;
 	
 	private readonly uint64 _rounds;
 	private readonly double _seconds;
